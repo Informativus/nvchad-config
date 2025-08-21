c806740bc84d5bb90cb7a64266ed3bfccccc296c
Иван <ivan@MacBook-Pro-Ivan.local>
2025-08-20 22:52:41 -0700

    fix: base

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -13,18 +13,6 @@ M.base46 = {
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
-    DiffChange = {
-      bg = "#464414",
-      fg = "none",
-    },
-    DiffAdd = {
-      bg = "#103507",
-      fg = "none",
-    },
-    DiffRemoved = {
-      bg = "#461414",
-      fg = "none",
-    },
   },
 }
 
